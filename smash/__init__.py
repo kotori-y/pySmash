62cef14c3ba65ca3187b44715751c80a74e9d17b
yzjkid9@gmail.com <yzjkid9@gmail.com>
2020-06-19 11:15:30 +0800

    :art: move p-value

--- smash/__init__.py
+++ smash/__init__.py
@@ -12,15 +12,40 @@ Created on Sun Jun 14 17:03:20 2020
 """
 
 
+from decimal import Decimal
 from multiprocessing import Pool
 import numpy as np
+import scipy as sc
 from .fingerprints import Morgan, Daylight
 
 
-# class MorganSmasher(Morgan):
-    
-#     def __init__(self, mols, **kwgrs):
-#         Morgan.__init__(self, mols=mols, **kwgrs)
+def Pvalue(n, m, ns, ms):
+    """
+    get P-value
+
+    Parameters
+    ----------
+    n : int
+        n compounds has m compounds with aspecific activity label.
+    m : int
+        n compounds has m compounds with aspecific activity label.
+    ns : int
+        a substructure is found in ns compounds.
+    ms : int
+        the amount of those compounds with the specific activity label is ms.
+
+    Yields
+    ------
+    res : float
+        P-value.
+
+    """
+    for i in range(ms, ns+1):
+        numerator = Decimal(sc.math.factorial(float(ns)))
+        denominatorA = Decimal(sc.math.factorial(float(i))) * Decimal(sc.math.factorial(float(ns-i)))
+        denominatorB = (m/n)**float(i)
+        denominatorC = (1 - (m/n))**(ns-i)
+        p_value = float(numerator/denominatorA) * denominatorB * denominatorC
+        yield p_value
         
-#     def GetMorganMatrix():
-#     	pass
+        
