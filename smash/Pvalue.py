5ec5cf9fb5616e2dfc17d51247db4e0cc48ad04e
yzjkid9@gmail.com <yzjkid9@gmail.com>
2020-06-19 11:05:27 +0800

    adjust p-value calculation

--- smash/Pvalue.py
+++ smash/Pvalue.py
@@ -12,18 +12,11 @@ Created on Thu Jun 11 16:48:16 2020
 """
 
 
-import pandas as pd
-import matplotlib.pyplot as plt
+import scipy as sc
+from decimal import Decimal
 
 
 
-def factorial(num):
-    if num == 0 or num == 1:
-        return 1
-    else:
-        return (num * factorial(num - 1))
-    
-    
 def Pvalue(n, m, ns, ms):
     """
     get P-value
@@ -46,43 +39,17 @@ def Pvalue(n, m, ns, ms):
 
     """
     for i in range(ms, ns+1):
-        part_a = factorial(ns)/(factorial(i) * factorial(ns-i))
-        part_b = (m/n)**i
-        part_c = (1-m/n)**(ns-i)
-        res = part_a * part_b * part_c
-        yield res
+        numerator = Decimal(sc.math.factorial(float(ns)))
+        denominatorA = Decimal(sc.math.factorial(float(i))) * Decimal(sc.math.factorial(float(ns-i)))
+        denominatorB = (m/n)**float(i)
+        denominatorC = (1 - (m/n))**(ns-i)
+        p_value = float(numerator/denominatorA) * denominatorB * denominatorC
+        yield p_value
         
 
-def solve(n, m, ns, ms = 1):
-    if 0 < sum(Pvalue(n,m,ns,ms)) <= 0.05:
-        return ms
-    elif sum(Pvalue(n,m,ns,ms)) == 0:
-        return None
-    else:
-        return solve(n, m, ns, ms+1)
-    
-
-def draw():
-    X = range(100)
-    y_acc = pd.Series([round(x*0.8,1) for x in X])
-    y_acc[y_acc < 1] = y_acc[y_acc >= 1].iloc[0]
-    
-    for ratio in [0.5, 0.7, 0.3]:
-        y_p = pd.Series([solve(100/ratio, 100, x) for x in X])
-        y_p[y_p.isna()] = y_p[y_p.notna()].iloc[0]
-        f,ax = plt.subplots()
-        ax.plot(X, y_acc, color='purple',lw=1)
-        ax.plot(X, y_p, color='red',lw=1)
-        ax.set_xticks(range(0,110,10))
-        ax.set_xlim([0,100])
-        ax.set_ylim([0,80])
-        ax.spines['right'].set_color('none')
-        ax.spines['top'].set_color('none')
-        ax.tick_params(direction='in', labelsize=8, length=4, colors='black')
-        plt.show()
-        
+     
 if '__main__' == __name__:
-    draw()
+    p_value = sum(Pvalue(50,10,10,5))
 
 
 
